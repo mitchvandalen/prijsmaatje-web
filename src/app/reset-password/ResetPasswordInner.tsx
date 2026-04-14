bb18aac433d735af97d4d76f2c1362b65033d13f
Mitch van Dalen <mitchvandalen@msn.com>
2026-04-14 15:52:02 +0200

    extra map wachtwoord reset

--- src/app/reset-password/ResetPasswordInner.tsx
+++ src/app/reset-password/ResetPasswordInner.tsx
@@ -1,16 +1,3 @@
-import ResetPasswordInner from "./ResetPasswordInner";
-
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ token?: string }>;
-}) {
-  const params = await searchParams;
-  const token = params?.token || "";
-
-  return <ResetPasswordInner token={token} />;
-}
-
 "use client";
 
 import { FormEvent, useState } from "react";
